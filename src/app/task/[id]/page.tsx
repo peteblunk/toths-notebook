6444fc386b3b9a84cc1b5708166b8ec82a8bb235
Pete Blunk <peteblunk@gmail.com>
2025-07-27 09:10:43 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/task/[id]/page.tsx
+++ src/app/task/[id]/page.tsx
@@ -3,7 +3,7 @@
 "use client";
 
 import React, { useEffect, useState } from 'react';
-import Link from 'link';
+import Link from 'next/link';
 import { notFound, useParams } from 'next/navigation';
 import { ArrowLeft } from 'lucide-react';
 

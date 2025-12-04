3ad551ae8324091bb240d9a92cf95dfb8da26252
Pete Blunk <peteblunk@gmail.com>
2025-12-04 05:55:16 +0000

    style: Add critical emoji to release notes

--- src/lib/ptah-config.ts
+++ src/lib/ptah-config.ts
@@ -1,18 +1,14 @@
 export const PTAH_CONFIG = {
-    // 1. CHANGE THIS whenever you deploy a new feature
-    version: "1.1.0", 
-    
-    // 2. This is the title of the task that will appear in Nun
-    releaseTitle: "GIFT OF PTAH: The Separation of Waters Version 1.1.0 - READ", 
-    
-    // 3. This is the content of the task (Markdown supported if you have it)
-    releaseNotes: `
-      - Implemented the 'Nun' interface: Incomplete tasks now float in the primordial waters (Chaos).
-      - Established the 'Ma'at' interface: Completed tasks are sanctified and are added to the foundation of universal Ma'at (Order) which it is our sacred duty to build. We thank you for your dutiful service.
-      - In these early days - you may expect frequent Gifts from Ptah.
-      - For trouble, seek guidance from: rites@unclepetelaboratories.net
-    `,
-    
-    // 4. Which category does it drop into? (Nun = Chaos/Incomplete)
-    targetCategory: "Today" 
-  };
+  version: "1.1.0", // Keep this the same! We are hijacking the current release.
+  
+  releaseTitle: "The Separation of Waters", 
+  
+  releaseNotes: `
+    - Implemented the 'Nun' interface: Incomplete tasks now float in the primordial waters (Chaos).
+    - Established the 'Ma'at' interface: Completed tasks are sanctified and are added to the foundation of universal Ma'at (Order) which it is our sacred duty to build. We thank you for your dutiful service. 😘
+    - In these early days - you may expect frequent Gifts from Ptah.
+    - For trouble, seek guidance from: rites@unclepetelaboratories.net
+  `,
+  
+  targetCategory: "Today" 
+};
